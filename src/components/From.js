62b5c6d2d6ff967f0d5cd9bff7eec88206f996af
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-21 15:49:02 -0600

    update to form.js

--- src/components/From.js
+++ src/components/From.js
@@ -13,6 +13,50 @@ function Form (props) {
         const valueToUse = type === 'checkbox' ? checked : value;
         change(name, valueToUse);
     };
+    return (
+        <form className="form container" onSubmit={onSubmit}>
+            <label>name
+                <input
+                    type='text'
+                    name='username'
+                    value={values.username}
+                    onChange={onChange}
+                    />
+            </label>
+
+            <label>email
+                <input
+                    type='email'
+                    name='email'
+                    value={values.email}
+                    onChange={onChange}
+                    />
+            </label>
+            { errors.email.length > 0 && <p className='error'>{errors.email}</p>}
+
+            <label>Set Password
+                <input
+                    type='password'
+                    name='password'
+                    value={values.password}
+                    onChange={onChange}
+                    placeholder='8-10 characters'
+                    />
+            </label>
+            {errors.password.length > 0 && <p className='error'>{errors.password}</p>}
+
+            <lables>Do you agree to the Terms of Services
+                <input 
+                    type='checkbox'
+                    name='termsOfService'
+                    value={values.termsOFService}
+                    onChange={onChange}
+                    required
+                    />
+            </lables>
+
+        </form>
+    )
 }
 
 export default Form
