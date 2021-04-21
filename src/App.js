07654135308ffa0d6e4ac8ee22f2690433616838
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-21 16:13:45 -0600

    axios .get to recieve data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/From';
 import axios from 'axios';
 import * as Yup from 'yup';
 
+const initialFormValues = {
+  username: '',
+  email: '',
+  password: '',
+  termsOfService: false,
+}
+
+const initialErrorValues ={
+  username: '',
+  email: '',
+  password: '',
+  termsOfService: false,
+}
+
+const initialUsers = [];
+const initialDisable = true;
+
+
 function App() {
+
+const [user, setUser] = useState(initialUsers);
+const [formValues, setFormValues] = useState(initialFormValues);
+const [formErrors, setFormErrors] =useState(initialErrorValues);
+const [disabled, setDisabled] = useState(initialDisable);
+
+axios
+  .get('https://reqres.in/api/users')
+  .then((res) => {
+    setUser(res.data.data);
+  })
+  .catch(err =>{
+
+    console.log(err);
+  })
+
   return (
     <div className="App">
-     
+     <Form 
+      values={formValues}
+      // change={inputChange}
+      // submit={formSubmit}
+      disabled={disabled}
+      errors ={formErrors}
+     />
     </div>
   );
 }
